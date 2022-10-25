a0b4e52c91a155d17d68427ea8001633567a8520
Henrique Leal da Matta <105172153+henriquelealm@users.noreply.github.com>
2022-10-24 21:58:18 -0300

    testes certos

--- comment/tests.py
+++ comment/tests.py
@@ -1,36 +1,35 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
-
-# Create your tests here.
 from django.utils import timezone
 
 from comment.forms import CommentForm
 from comment.models import Comment
 
 
-class CommentTest(TestCase):
+class teste_comentario(TestCase):
 
     def setUp(self):
-        self.user = get_user_model().objects.create_user(username='test_user', password='testing',
-                                                         email='test@example.com')
+        self.user = get_user_model().objects.create_user(username='teste_usuario', password='teste123',
+                                                         email='teste@exemplo.com')
         self.user.save()
         self.date = timezone.now()
-        self.comment = Comment(user=self.user, content='description of the comment',
+        self.comment = Comment(user=self.user,
+                               content='teste do conteudo do comentario',
                                data=self.date)
         self.comment.save()
 
     def tearDown(self):
         self.user.delete()
 
-    def test_read_comment(self):
+    def test_ler_comentario(self):
         self.assertEqual(self.comment.user, self.user)
-        self.assertEqual(self.comment.content, 'description of the comment')
+        self.assertEqual(self.comment.content, 'teste do conteudo do comentario')
         self.assertEqual(self.comment.data, self.date)
 
-    def test_update_comment_content(self):
-        self.comment.description = 'new description'
+    def test_atualizar_comentario(self):
+        self.comment.description = 'novo comentario'
         self.comment.save()
-        self.assertEqual(self.comment.description, 'new description')
+        self.assertEqual(self.comment.description, 'novo comentario')
 
     def test_valid_form(self):
         comment = Comment.objects.create(content='content', user=self.user, data=self.date)
@@ -39,7 +38,6 @@ class CommentTest(TestCase):
         self.assertTrue(form.is_valid())
 
     def test_invalid_form(self):
-        # trying to save data without date
         comment = Comment.objects.create(content='content', user=self.user, data=self.date)
         data = {'content': comment.content, 'user': comment.user}
         form = CommentForm(data=data)
--- main/models.py
+++ main/models.py
@@ -1,8 +1,2 @@
 from django.db import models
 from django.contrib.auth.models import User
-
-# Create your models here.
-class User(models.Model):
-
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    
