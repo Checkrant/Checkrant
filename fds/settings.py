c0b384993ca154b1d41dd90ac28fb15c04e9b388
C4BrrT <ccbc@cesar.school>
2022-11-16 18:31:43 -0300

    settings update

--- fds/settings.py
+++ fds/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-odg$*_!5*220uk$k)9jn8+62zok)qhv07v-jil%hdcgu0(w4s3
 DEBUG = True
 
 ALLOWED_HOSTS =['https://check-restaurant.herokuapp.com/']
-CSRF_TRUSTED_ORIGINS = ['https://check-restaurant.herokuapp.com/', 'https://check-restaurant.herokuapp.com']
+CSRF_TRUSTED_ORIGINS = ['https://check-restaurant.herokuapp.com/', 'https://check-restaurant.herokuapp.com', 'https://checkrant.herokuapp.com', 'https://checkrant.herokuapp.com/']
 
 # Application definition
 
