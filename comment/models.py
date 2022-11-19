189e40ae4700bb12828cc56f3aa19d1b7aa6743e
C4BrrT <ccbc@cesar.school>
2022-11-19 19:08:12 -0300

    fix comment

--- comment/models.py
+++ comment/models.py
@@ -5,6 +5,4 @@ from django.contrib.auth.models import User
 class Comment(models.Model):
     data =  models.DateTimeField(max_length=256)
     content = models.TextField(max_length=350)
-    
-    def __str__(self) -> str:
-        return self.content
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
