db4d1fdd066c9eb6269acbdcad2ae2e746301625
C4BrrT <ccbc@cesar.school>
2022-11-21 18:50:40 -0300

    comment test

--- comment/tests.py
+++ comment/tests.py
@@ -9,20 +9,14 @@ from comment.models import Comment
 class teste_comentario(TestCase):
 
     def setUp(self):
-        self.user = get_user_model().objects.create_user(username='teste_usuario', password='teste123',
-                                                         email='teste@exemplo.com')
-        self.user.save()
         self.date = timezone.now()
-        self.comment = Comment(user=self.user,
-                               content='teste do conteudo do comentario',
-                               data=self.date)
+        self.comment = Comment(content='teste do conteudo do comentario', data=self.date)
         self.comment.save()
 
     def tearDown(self):
-        self.user.delete()
+        self.comment.delete()
 
     def test_ler_comentario(self):
-        self.assertEqual(self.comment.user, self.user)
         self.assertEqual(self.comment.content, 'teste do conteudo do comentario')
         self.assertEqual(self.comment.data, self.date)
 
@@ -32,13 +26,13 @@ class teste_comentario(TestCase):
         self.assertEqual(self.comment.description, 'novo comentario')
 
     def test_valid_form(self):
-        comment = Comment.objects.create(content='content', user=self.user, data=self.date)
-        data = {'content': comment.content, 'user': comment.user, 'data': comment.data}
+        comment = Comment.objects.create(content='content', data=self.date)
+        data = {'content': comment.content, 'data': comment.data}
         form = CommentForm(data=data)
         self.assertTrue(form.is_valid())
 
     def test_invalid_form(self):
-        comment = Comment.objects.create(content='content', user=self.user, data=self.date)
-        data = {'content': comment.content, 'user': comment.user}
+        comment = Comment.objects.create(content='content', data=self.date)
+        data = {'content': comment.content}
         form = CommentForm(data=data)
         self.assertFalse(form.is_valid())
